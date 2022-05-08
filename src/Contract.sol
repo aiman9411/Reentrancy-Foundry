78b8689e01e8b499bee12aafedc3a40035baa6d5
Aiman Nazmi <aimannazmi59@gmail.com>
2022-05-09 00:36:39 +0800

    add BankApp contract

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Contract {}
+contract BankApp {
+
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    function withdraw() external {
+        uint256 balance = balances[msg.sender];
+        require(balance > 0, "Insufficient balance");
+        (bool success, ) = msg.sender.call{value: balance}("");
+        require(success, "Failed to send");
+        balances[msg.sender] = 0;
+    }
+}
